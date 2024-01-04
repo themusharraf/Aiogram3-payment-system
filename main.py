72292530816a1d2e2740d526fc838ae094f13d77
allnc <meibrohimov@gmail.com>
2024-01-05 00:22:01 +0500

    aiogram 3

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from aiogram import Bot
 from aiogram.types import Message, LabeledPrice
 
+
+async def order(message: Message, bot: Bot):
+    await bot.send_invoice(
+        chat_id=message.chat_id,
+        title="Order alert Telegram bot",
+        description="Order alert Telegram bot price product payment ",
+        payload="Payment bot orqali",  # noqa
+    )
